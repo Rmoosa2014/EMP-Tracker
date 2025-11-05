4b15f18faf4888b822e54a274398ff76b6c63964
Rmoosa2014 <riyasmosa@gmail.com>
2025-11-05 11:58:27 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
+// App.js (after)
 import React from 'react';
 import PreviewScreen from './components/PreviewScreen';
 
 const DOCUMENT_URL = 'https://i.imgur.com/mRdMBZA.png';
 
-const App: React.FC = () => {
+const App = () => { // <--- TYPE REMOVED
   return (
     <div className="min-h-screen text-white font-sans">
       <PreviewScreen documentUrl={DOCUMENT_URL} />
